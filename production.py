b374aba7b496c6c3e0e7c2d925f61b20a67638b0
Luiz F. C. Martins <239121271+luiz1361@users.noreply.github.com>
2026-01-15 18:14:54 +0000

    Initial commit

---
+++ production.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+def get_user_data(user_id):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    # SEMGREP TRIGGER: String formatting into an execute call
+    # Rule: python.lang.security.audit.sqli.sqlite-string-format
+    query = "SELECT * FROM users WHERE id = %s" % user_id
+    cursor.execute(query)
+
+    return cursor.fetchone()
